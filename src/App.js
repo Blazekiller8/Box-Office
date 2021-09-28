4430cc41d24837cc9452331798dae6cadcae532b
Blazekiller8 <sivaraamtk@hotmail.com>
2021-09-28 22:27:04 +0530

    added page components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom'
+import Navs from './compnents/Navs';
+import Actors from './pages/Actors';
+import Home from './pages/Home';
 
 function App() {
   return (
+    <div>
+      <Navs />
     <Switch >
       <Route exact path="/">
-        This is Homepage
+        <Home />
       </Route>
       <Route exact path="/actors">
-        This is Actor
+        <Actors />
       </Route>
       <Route>
         This is 404 Page not Found!
       </Route>
     </Switch>
+    </div>
   );
 }
 
---
+++ src/pages/Actors.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Actors = () => {
+    return (
+        <div>
+            These are actors
+        </div>
+    )
+}
+
+export default Actors
