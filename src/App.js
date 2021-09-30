02622b6161e23a33799af66b899399763978aa6b
Blazekiller8 <sivaraamtk@hotmail.com>
2021-09-30 22:08:34 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Starred from './pages/Starred';
 import Home from './pages/Home';
+import Show from './pages/Show';
 
 function App() {
   return (
@@ -14,6 +15,10 @@ function App() {
           <Starred />
         </Route>
         
+        <Route exact path="/show/:id">
+          < Show />
+        </Route>
+
         <Route>This is 404 Page not Found!
         </Route>
       </Switch>
